b97a6451c881c809d43fd0d5e2b53fb9fa2bac6e
Yaroslav <yaroslav>
2024-10-25 18:29:55 +0300

    Initial Commit

---
+++ DonutApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DonutApp2
+//
+//  Created by Yaroslav on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
